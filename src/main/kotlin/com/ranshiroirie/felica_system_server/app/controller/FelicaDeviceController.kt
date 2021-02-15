393cba1dc678b07047f370dcda21d8e5c1263347
ranshiroirie <ranshiroirie@gmail.com>
2021-02-15 22:18:37 +0900

    Felicaデバイス管理用DB「felica_device」のWebApiの修正とエラー処理の機能追加 on 2021.2.15

--- src/main/kotlin/com/ranshiroirie/felica_system_server/app/controller/FelicaDeviceController.kt
+++ src/main/kotlin/com/ranshiroirie/felica_system_server/app/controller/FelicaDeviceController.kt
@@ -3,21 +3,23 @@ package com.ranshiroirie.felica_system_server.app.controller
 import com.ranshiroirie.felica_system_server.domain.entity.FelicaDevice
 import com.ranshiroirie.felica_system_server.domain.service.felica_device.FelicaDeviceService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 
 @RestController
+@RequestMapping("/felica_device")
 class FelicaDeviceController {
 
     @Autowired
     lateinit var felicaDeviceService: FelicaDeviceService
 
-    @GetMapping("/felica_device/get/all")
-    fun getFelicaDeviceList(): List<FelicaDevice>{
-        return felicaDeviceService.getFelicaDeviceList()
+    @GetMapping("/get/all")
+    fun getFelicaDeviceList(): ResponseEntity<List<FelicaDevice>>{
+        return ResponseEntity.ok(felicaDeviceService.getFelicaDeviceList())
     }
 
-    @GetMapping("/felica_device/get/card_info")
+    @GetMapping("/get/card_info")
     fun getFelicaDeviceByCardInfo(
         @RequestParam ("device_name", required=true)device_name: String,
         @RequestParam ("card_id", required=true)card_id: String,
@@ -27,24 +29,28 @@ class FelicaDeviceController {
         return felicaDeviceService.getFelicaDeviceByCardInfo(device_name, card_id, card_pmm, card_sys)
     }
 
-    @GetMapping("/felica_device/get/felica_id")
+    @GetMapping("/get/felica_id")
     fun getFelicaDeviceById(
         @RequestParam ("felica_id",required=true)felica_id: Long
     ): FelicaDevice{
         return felicaDeviceService.getFelicaDeviceById(felica_id)
     }
 
-    @PostMapping("/felica_device/add")
+    @PostMapping("/add")
     fun createFelicaDeviceInfo(
         @RequestParam ("device_name", required=true)device_name: String,
         @RequestParam ("card_id", required=true)card_id: String,
         @RequestParam ("card_pmm", required=true)card_pmm: String,
         @RequestParam ("card_sys", required=true)card_sys: String
-    ):FelicaDevice{
-        return felicaDeviceService.getInsertFelicaDeviceInfo(device_name, card_id, card_pmm, card_sys)
+    ):ResponseEntity<FelicaDevice>{
+        return ResponseEntity.ok(felicaDeviceService.getInsertFelicaDeviceInfo(device_name, card_id, card_pmm, card_sys))
     }
 
-    @PutMapping("/felica_device/update/felica_id")
+    @ExceptionHandler(java.sql.SQLIntegrityConstraintViolationException::class)
+    fun exceptionInsertCardInfoExisted(e: java.sql.SQLIntegrityConstraintViolationException) : Map<String, String?> =
+        mapOf(Pair("error", e.toString()), Pair("message", "Card Info inserted has been existed. 入力されたカード情報は既に存在しています。"))
+
+    @PutMapping("/update/felica_id")
     fun updateFelicaDeviceNameById(
         @RequestParam ("felica_id",required=true)felica_id: Long,
         @RequestParam ("device_name", required=true)device_name: String
@@ -52,7 +58,7 @@ class FelicaDeviceController {
         return felicaDeviceService.getUpdateFelicaDeviceNameById(felica_id, device_name)
     }
 
-    @DeleteMapping("/felica_device/delete/felica_id")
+    @DeleteMapping("/delete/felica_id")
     fun deleteFelicaDeviceById(
         @RequestParam ("felica_id",required=true)felica_id: Long
     ):FelicaDevice{
